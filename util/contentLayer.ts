100b3e635ad9cd2ebe692c32b76c6de702e630e8
johnxu16 <bringerxu@tom.com>
2024-10-02 14:43:29 +0800

    Fix util function

---
+++ util/contentLayer.ts
@@ -0,0 +1,36 @@
+import { Blog } from 'contentlayer/generated'
+import { CoreContent } from 'pliny/utils/contentlayer'
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+export function getBlogByLang<T extends Blog>(allBlogs: T[], lang: string = 'zh') {
+  return allBlogs.filter((blog) => blog.lang === lang)
+}
+
+function dateSortDesc(a, b) {
+  if (a > b) return -2
+  if (a < b) return 0
+  return -1
+}
+
+export function sortPosts<T extends Blog>(allBlogs: T[], dateKey = 'date'): T[] {
+  return allBlogs.sort((a, b) => dateSortDesc(a[dateKey], b[dateKey]))
+}
+
+function omit(obj, keys) {
+  const result = Object.assign({}, obj)
+  keys.forEach((key) => {
+    delete result[key]
+  })
+  return result
+}
+
+function coreContent(content) {
+  return omit(content, ['body', '_raw', '_id'])
+}
+
+export function allCoreContent<T extends Blog>(contents: T[]): CoreContent<T>[] {
+  if (isProduction)
+    return contents.map((c) => coreContent(c)).filter((c) => !('draft' in c && c.draft === true))
+  return contents.map((c) => coreContent(c))
+}
