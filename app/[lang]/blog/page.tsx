8b624e16fda9b2d230039e88e4a546ed45d6aa22
johnxu16 <bringerxu@tom.com>
2024-10-06 12:11:12 +0800

    Fix pagination

--- app/[lang]/blog/page.tsx
+++ app/[lang]/blog/page.tsx
@@ -1,14 +1,14 @@
 import ListLayout from '@/layouts/ListLayoutWithTags'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
-import { sortPosts, allCoreContent, getBlogByLang } from 'util/contentLayer'
+import { sortPosts, allCoreContent } from 'util/contentLayer'
 
 const POSTS_PER_PAGE = 5
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
 export default function BlogPage() {
-  const posts = allCoreContent(sortPosts(getBlogByLang(allBlogs)))
+  const posts = allCoreContent(sortPosts(allBlogs))
   const pageNumber = 1
   const initialDisplayPosts = posts.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,9 @@
-import { allCoreContent, getBlogByLang, sortPosts } from 'util/contentLayer'
+import { allCoreContent, sortPosts } from 'util/contentLayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from '../Main'
 
 export default async function Page({ params: { lang } }) {
-  const sortedPosts = sortPosts(getBlogByLang(allBlogs, lang))
+  const sortedPosts = sortPosts(allBlogs, lang)
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
 }
